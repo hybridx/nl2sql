2d5fb904902299e56c7b32102fb5875a54bab8ae
Rohit Bharmal <rbharmal@redhat.com>
2025-03-27 21:21:49 +0530

    deleted next js app

--- frontend/src/app/api/tags/route.ts
+++
@@ -1,9 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-export async function GET(req: Request) {
-  const OLLAMA_URL = "http://localhost:11434";
-  console.log("OLLAMA_URL:", OLLAMA_URL);
-  const res = await fetch(OLLAMA_URL + "/api/tags");
-  return new Response(res.body, res);
-}
