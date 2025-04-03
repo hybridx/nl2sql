155d3528c6a0289b6c1d33a6f02f83b387f3122b
Rohit Bharmal <rbharmal@redhat.com>
2025-04-03 16:00:29 +0530

    created components for better code readibilty and analysis ui changes

---
+++ frontend/src/components/QueryForm.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, TextInput, Checkbox } from "@patternfly/react-core";
+
+const QueryForm = ({
+  userInput,
+  setUserInput,
+  enableAnalysis,
+  setEnableAnalysis,
+  onSubmit,
+}) => {
+  return (
+    <>
+      <div className="w-full rounded mb-2">
+        <TextInput
+          type="text"
+          aria-label="Ask a question"
+          placeholder="Ask a question..."
+          value={userInput}
+          onChange={(_event, value) => setUserInput(value)}
+        />
+      </div>
+      <div className="mb-4">
+        <Checkbox
+          label="Enable Analysis"
+          isChecked={enableAnalysis}
+          onChange={(_event, checked) => setEnableAnalysis(checked)}
+          id="enable-analysis"
+        />
+      </div>
+
+      <div className="flex items-center justify-center mb-4">
+        <Button variant="primary" onClick={onSubmit}>
+          Submit
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default QueryForm;
